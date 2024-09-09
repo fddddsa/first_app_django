85b2c9dbd65d8b1b4ac731c097f04be1ece2e14e
Your Name <you@example.com>
2024-09-09 12:04:49 +0300

    09.09.24 [ЛЗ3 - установил Django, сделал папку hello,сделал первый проект на Django]

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -36,7 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles'
+    'django.contrib.adrnin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
+    'firstapp',
 ]
 
 MIDDLEWARE = [
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -16,7 +16,5 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from firstapp import views
+urlpatterns = path ( 'admin/', admin. site. urls),
